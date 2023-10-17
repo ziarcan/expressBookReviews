4fe183739f8f4dd310b893be950d4532b76d3a92
Branislav Balaz <branislav.balaz@xit.camp>
2023-10-17 22:41:26 +0200

    tasks from 10 to 13 implemented (Promises)

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -50,7 +50,6 @@ public_users.get('/author/:author', function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-    //Write your code here
     const title = req.params.title;
     const filteredBooks = Object.values(books).filter(item => item.title === title);
     if(filteredBooks.length) {
@@ -70,5 +69,68 @@ public_users.get('/review/:isbn', function (req, res) {
         return res.status(404).json({message: `No book with ISBN ${isbn} found`});
     }
 });
+
+// TASK 10 - Get the book list available in the shop using Promises
+public_users.get('/books',function (req, res) {
+
+    const allBooks = new Promise(resolve => {
+        resolve(res.send(books));
+    });
+
+    allBooks.then(() => console.log("Task 10: promise resolved"));
+
+});
+
+// TASK 11 - Get book details based on ISBN using Promises
+public_users.get('/books/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+
+    const bookDetail = new Promise((resolve, reject) => {
+        if (book) {
+            resolve(res.send(book));
+        } else {
+            reject(res.status(404).json({message: `No book with ISBN ${isbn} found`}));
+        }
+    });
+
+    bookDetail.then(() => console.log("Task 11: promise resolved")).catch(() => console.log("Task 11: promise rejected"));
+
+});
+
+// TASK 12 - Get book details based on author using Promises
+public_users.get('/books/author/:author', function (req, res) {
+    const author = req.params.author;
+    const filteredBooks = Object.values(books).filter(item => item.author === author)
+
+    const bookDetail = new Promise((resolve, reject) => {
+        if(filteredBooks.length) {
+            resolve(res.send(filteredBooks));
+        } else {
+            reject(res.status(404).json({message: `No book for author ${author} found`}));
+        }
+    });
+
+    bookDetail.then(() => console.log("Task 12: promise resolved")).catch(() => console.log("Task 12: promise rejected"));
+});
+
+// TASK 13 - Get all books based on title using Promises
+public_users.get('/books/title/:title', function (req, res) {
+    const title = req.params.title;
+    const filteredBooks = Object.values(books).filter(item => item.title === title);
+
+    const bookDetail = new Promise((resolve, reject) => {
+        if(filteredBooks.length) {
+            resolve(res.send(filteredBooks));
+        } else {
+            reject(res.status(404).json({message: `No book for title ${title} found`}));
+        }
+    });
+
+    bookDetail.then(() => console.log("Task 13: promise resolved")).catch(() => console.log("Task 13: promise rejected"));
+});
+
+
+
 
 module.exports.general = public_users;
